97be3b4c6fcd4597f437393531c124987aad6727
sanyochek1 <tyryskinsasa4@gmail.com>
2024-12-12 23:49:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static string ReverseString(string input)
+    {
+        // преобразование строки в массив из символов, реверсирование
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Домашнее задание №2");
+        // примеры для тестирования метода
+        string[] testStrings =
+        {
+            "Hello, World!",
+            "приветпока",
+            "а роза упала на лапу азора",
+            "qwertyuiop",
+            "когда-нибудь я сделаю домашку по С# на максимум",
+            ""
+        };
+
+        // тестирование метода
+        foreach (string test in testStrings)
+        {
+            string reversedStr = ReverseString(test);
+            Console.WriteLine($"Исходная строка: '{test}' | Реверсированная строка: '{reversedStr}'");
+        }
+    }
+}
